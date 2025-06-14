21317d696a7c4c01aa31f94dec01bb22b57bdf33
Rafael München <73946640+rafaelmuenchen@users.noreply.github.com>
2025-06-14 02:41:15 -0300

    Update Altair.tsx

--- src/components/altair/Altair.tsx
+++ src/components/altair/Altair.tsx
@@ -20,6 +20,8 @@ import {
   FunctionDeclaration,
   LiveServerToolCall,
   Modality,
+  StartSensitivity,
+  EndSensitivity,
   Type,
 } from "@google/genai";
 
@@ -46,14 +48,29 @@ function AltairComponent() {
   useEffect(() => {
     setModel("models/gemini-2.0-flash-exp");
     setConfig({
-      responseModalities: [Modality.AUDIO],
+      responseModalities: [Modality.TEXT],
+      inputAudioTranscription: {
+        enabled: true,
+        languageCode: "pt-BR",
+        model: "nova-2-flash-001",
+        enableAutomaticPunctuation: true,
+      },
+      realtimeInputConfig: {
+        automaticActivityDetection: {
+          disabled: false, // default
+          startOfSpeechSensitivity: StartSensitivity.START_SENSITIVITY_LOW,
+          endOfSpeechSensitivity: EndSensitivity.END_SENSITIVITY_LOW,
+          // prefixPaddingMs: 20,
+          silenceDurationMs: 100,
+        }
+      },
       speechConfig: {
         voiceConfig: { prebuiltVoiceConfig: { voiceName: "Aoede" } },
       },
       systemInstruction: {
         parts: [
           {
-            text: 'You are my helpful assistant. Any time I ask you for a graph call the "render_altair" function I have provided you. Dont ask for additional information just make your best judgement.',
+            text: 'Você é um sistema de transcrição de voz em tempo real altamente eficiente. Sua tarefa exclusiva é transcrever o áudio que está sendo transmitido continuamente. Seu resultado deve ser estritamente o texto transcrito. Produza apenas as palavras faladas, mantendo o output livre de quaisquer comentários, explicações ou metadados. O áudio será fornecido em um fluxo contínuo. A cada 100 milisegundos de áudio processado, retorne o trecho de texto transcrito correspondente até aquele momento. Inicie a transcrição e o envio dos resultados imediatamente com o início do áudio, processando o fluxo de forma contínua, sem aguardar por pausas ou pelo término da transmissão. Concentre-se em fornecer a transcrição com a máxima velocidade, mesmo que as primeiras versões de um trecho sejam refinadas posteriormente com mais contexto. Se o áudio parar ou a conexão for interrompida, finalize a transcrição do último trecho recebido. Seu output deve ser exclusivamente o texto transcrito.',
           },
         ],
       },
@@ -61,7 +78,7 @@ function AltairComponent() {
         // there is a free-tier quota for search
         { googleSearch: {} },
         { functionDeclarations: [declaration] },
-      ],
+      ]
     });
   }, [setConfig, setModel]);
 
