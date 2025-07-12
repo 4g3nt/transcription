60e11d8a51759d601c2d368fa59ae736969cfba7
rafaelmuenchen <rafa@tekr.io>
2025-07-12 00:42:20 -0300

    Reduce silence duration in Altair component from 2000ms to 1500ms for improved speech detection responsiveness.

--- src/components/altair/Altair.tsx
+++ src/components/altair/Altair.tsx
@@ -61,7 +61,7 @@ function AltairComponent() {
           // startOfSpeechSensitivity: StartSensitivity.START_SENSITIVITY_HIGH,
           // endOfSpeechSensitivity: EndSensitivity.END_SENSITIVITY_LOW,
           // prefixPaddingMs: 20,
-          silenceDurationMs: 2000,
+          silenceDurationMs: 1500,
         }
       },
       speechConfig: {
